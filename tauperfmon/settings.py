81e7fa780cc9d2f8757ea3f40b7f95c6dda4f929
Noel Dawe <Noel.Dawe@cern.ch>
2011-08-31 17:21:58 -0700

    updates

--- tauperfmon/settings.py
+++ tauperfmon/settings.py
@@ -102,10 +102,13 @@ MIDDLEWARE_CLASSES = (
 
 ROOT_URLCONF = 'tauperfmon.urls'
 
+import os
+dirname = os.path.dirname(globals()["__file__"])
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(dirname, 'admin/templates'),
 )
 
 INSTALLED_APPS = (
