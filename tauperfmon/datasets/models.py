413f2f921497c249f036aa1bec2986f7d82de788
Noel Dawe <Noel.Dawe@cern.ch>
2011-09-01 12:00:38 -0700

    updates

--- tauperfmon/datasets/models.py
+++ tauperfmon/datasets/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Dataset(models.Model):
+
+    name = models.CharField(max_length=100, unique=True) 
+    
+    TYPE_CHOICES = (
+        ('I', 'Integer'),
+        ('F', 'Float')
+    )
+    type = models.CharField(max_length=1, choices=TYPE_CHOICES, default='F')
+
+    def __unicode__(self):
+
+        return self.name
--- tauperfmon/discriminants/models.py
+++ tauperfmon/discriminants/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Discriminant(models.Model):
+
+    name = models.CharField(max_length=100, unique=True) 
+    METHOD_CHOICES = (
+        ('CUTS', 'Cut-based'),
+        ('BDT', 'Boosted Decision Trees'),
+        ('LLH', 'Likelihood')
+    )
+    type = models.CharField(max_length=4, choices=METHOD_CHOICES)
+    min = models.FloatField(default=0.)
+    max = models.FloatField(default=1.)
+
+    def __unicode__(self):
+
+        return "%s: %s" % (self.type, self.name)
