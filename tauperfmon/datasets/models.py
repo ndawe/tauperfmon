d683e1d67bb77231f176ff6b88ba3499e93c7f0a
Noel Dawe <Noel.Dawe@cern.ch>
2011-09-01 12:09:47 -0700

    updates

--- tauperfmon/datasets/models.py
+++ tauperfmon/datasets/models.py
@@ -5,10 +5,26 @@ class Dataset(models.Model):
     name = models.CharField(max_length=100, unique=True) 
     
     TYPE_CHOICES = (
-        ('I', 'Integer'),
-        ('F', 'Float')
+        ('DATA', 'Data'),
+        ('MC', 'Monte Carlo')
     )
-    type = models.CharField(max_length=1, choices=TYPE_CHOICES, default='F')
+    data_type = models.CharField(max_length=4, choices=TYPE_CHOICES, default='MC')
+    
+    CLASS_CHOICES = (
+        ('SIGNAL', "Signal"),
+        ('BACKGROUND', 'Background')
+    )
+    data_class = models.CharField(max_length=10, choices=CLASS_CHOICES, default='BACKGROUND')
+
+    LABEL_CHOICES = (
+        ('TAU', "Tau"),
+        ('MUON', "Muon"),
+        ('ELEC', "Electron"),
+        ('JET', 'Jet background')
+    )
+    label = models.CharField(max_length=4, choices=LABEL_CHOICES, default='JET')
+
+    weight = models.FloatField(default=1.)
 
     def __unicode__(self):
 
