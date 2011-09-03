8425c469ec4358768049d27f4bbb406f0b34ca73
Noel Dawe <Noel.Dawe@cern.ch>
2011-09-03 00:00:18 -0700

    updates

--- tauperfmon/urls.py
+++ tauperfmon/urls.py
@@ -8,9 +8,9 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'tauperfmon.views.home', name='home'),
     # url(r'^tauperfmon/', include('tauperfmon.foo.urls')),
-
+    url(r'^variables', 'variables.views.variable_list'),
     # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
--- tauperfmon/variables/views.py
+++ tauperfmon/variables/views.py
@@ -1,1 +1,6 @@
-# Create your views here.
+from variables.models import Variable
+from django.shortcuts import render_to_response
+
+def variable_list(request):
+    variables = Variable.objects.all()
+    return render_to_response('variables/index.html', {'variables': variables})
